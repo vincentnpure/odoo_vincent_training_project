1a88c6c1ee03245f9a4513d15243fda562370403
Vincent74230 <vincent.nowak@hotmail.fr>
2022-06-23 15:17:20 +0200

    ADDED simple custom module

---
+++ custom_module/__manifest__.py
@@ -0,0 +1,23 @@
+# _*_ coding:utf-8 _*_
+
+{
+    'name': 'Custom module',
+    'summary': """Training module""",
+    'description': """
+        Module to manage training:
+        _Models
+        _fields
+    """,
+    'Author' : 'Vincentn',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data':[
+
+    ],
+    'demo': [
+
+    ],
+
+}
